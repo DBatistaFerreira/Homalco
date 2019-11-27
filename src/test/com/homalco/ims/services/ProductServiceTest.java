a6b6ad4d045f8d69ff0af55e801fd5fc016f473a
Daniel <danielwiktorczyk@gmail.com>
2019-11-27 13:36:58 -0500

    Added test directory, tests

---
+++ src/test/com/homalco/ims/services/ProductServiceTest.java
@@ -0,0 +1,20 @@
+package com.homalco.ims.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductServiceTest {
+
+    @Test
+    public void saveProduct() {
+    }
+
+    @Test
+    public void deleteProduct() {
+    }
+
+    @Test
+    public void getProduct() {
+    }
+}
