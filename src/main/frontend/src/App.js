965f8e8db9f1036c3c8d447b2979520bf8d41771
Waqar Qureshi <qureshi.waqarh@yahoo.com>
2019-11-27 22:58:15 -0500

    Testing axios and token grab

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Login from "./Login";
 import Main from "./Main";
--- src/main/frontend/src/Login.js
+++ src/main/frontend/src/Login.js
@@ -3,10 +3,13 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import imsLogo from './imsLogo.png';
+import axios from 'axios';
+
 class Login extends Component {
 
     constructor(props){
         super(props);
+        this.login = this.login.bind(this);
         this.state={
             username:'',
             password:''
@@ -24,20 +27,58 @@ class Login extends Component {
                             hintText="Enter your Username"
                             floatingLabelText="Username"
                             onChange = {(event,newValue) => this.setState({username:newValue})}
+                            id = 'name'
                         />
                         <br/>
                         <TextField
                             type="password"
                             hintText="Enter your Password"
                             floatingLabelText="Password"
+                            id='pass'
                             onChange = {(event,newValue) => this.setState({password:newValue})}
                         />
                         <br/>
-                        <RaisedButton label="Sign In" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                        <RaisedButton label="Sign In" primary={true} style={style} onClick={this.login}/>
                     </div>
                 </MuiThemeProvider>
             </div>
         );
+    }
+
+
+    login() {
+        let name;
+        let pass;
+        let element;
+        element = document.getElementById('name');
+        if (element != null) {
+            name = element.value;
+
+        }
+        else {
+            name = null;
+        }
+        element = document.getElementById('pass');
+        if (element != null) {
+            pass = element.value;
+        }
+        else {
+            pass = null;
+        }
+
+        axios.post('http://localhost:8080/login', {
+            username: name,
+            password: pass
+        }).then(res => {
+            console.log(res.headers.get('authorization'));
+            if (this.state.loggedIn === true) {
+                //alert("logged in"); // login returns true
+                this.props.history.push("/IMS/");
+            }
+        }, err => {
+            alert("Server rejected response: " + err);
+        });
+
     }
 }
 const style = {
