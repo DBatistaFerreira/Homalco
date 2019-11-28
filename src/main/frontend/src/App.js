f186772128c0fd1c77e690c170555e469f6118c9
Daniel Batista-Ferreira <danielbatfer@hotmail.com>
2019-11-27 20:14:33 -0500

    Adds Product UI component

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Login from "./Login";
 import Main from "./Main";
+import Product from "./Product";
 
 function App() {
   return (
     <div className="App">
         <Login/>
         <Main/>
+        <Product/>
     </div>
   );
 }
