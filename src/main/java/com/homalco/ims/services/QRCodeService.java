a09e46b29780b83b30cafe26833a32b4a7eafed9
Jake Bedard <bedardjake@gmail.com>
2019-11-27 20:04:55 -0500

    Add unit test for creating a QR code

--- src/main/java/com/homalco/ims/services/QRCodeService.java
+++ src/main/java/com/homalco/ims/services/QRCodeService.java
@@ -1,20 +1,24 @@
 package com.homalco.ims.services;
 
+import lombok.Getter;
 import net.glxn.qrgen.javase.QRCode;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 
 @Service
+@Getter
 public class QRCodeService{
 
+    private File QRCodeImage;
+
     public void createQRCode(String id) {
-        File QRcodeImage = QRCode.from(id).file("product_" + String.join("_", id.toLowerCase().split("\\s")) + "_");
-        printQRCode(QRcodeImage);
+        this.QRCodeImage = QRCode.from(id).file("product_" + String.join("_", id.toLowerCase().split("\\s")) + "_");
+        printQRCode();
     }
 
-    private void printQRCode(File QRcodeImage) {
+    public void printQRCode() {
 //      TODO: print to a printer
-        System.out.println(QRcodeImage.getAbsolutePath());
+        System.out.println(this.QRCodeImage.getAbsolutePath());
     }
 }
